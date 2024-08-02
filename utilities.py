ea342542b31cde6ac37c27f659aec32706383455
g-pengrend <126435967+g-pengrend@users.noreply.github.com>
2024-08-03 00:46:45 +0800

    Revert "updated html loader"
    This reverts commit 42e20f6072caff5e10bc7f69548cfccfc7dc1ebf.

--- utilities.py
+++ utilities.py
@@ -9,52 +9,52 @@ def get_filename_from_cd(cd):
     """
     if not cd:
         return None
-    fname = re.findall('filename="([^"]+)"', cd)
-    if len(fname) == 0:
-        return None
-    fname = fname[0]
+    fname = cd.split('filename=')[1]
     if fname.lower().startswith(("utf-8''", "utf-8'")):
         fname = fname.split("'")[-1]
     return unquote(fname)
-
+  
 def download_file(url):
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
-        cd = r.headers.get('content-disposition')
-        filename = get_filename_from_cd(cd)
+        filename = get_filename_from_cd(r.headers.get('content-disposition'))
         if not filename:
-            # Use the URL to create a filename if no content-disposition header is present
-            parsed_url = urlparse(url)
-            filename = os.path.basename(parsed_url.path)
-            if not filename:
-                filename = parsed_url.netloc.replace('.', '_')
+            filename = urlparse(url).geturl().replace('https://', '').replace('/', '-')
         filename = 'content/' + filename
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
         return filename
-
+      
 def readtext(path):
-    path = path.rstrip().replace(' \n', '').replace('%0A', '')
-    if re.match(r'^https?://', path):
-        filename = download_file(path)
-    else:
-        filename = os.path.abspath(path)
+  path = path.rstrip()
+  path = path.replace(' \n', '')
+  path = path.replace('%0A', '')
+  if re.match(r'^https?://', path):
+    filename = download_file(path)
+  else:
     
-    filetype = magic.from_file(filename, mime=True)
-    print(f"\nEmbedding {filename} as {filetype}")
+    relative_path = path
+    filename = os.path.abspath(relative_path)
+  
+  filetype = magic.from_file(filename, mime=True)
+  print(f"\nEmbedding {filename} as {filetype}")
+  text = ""
+  if filetype == 'application/pdf':
+    text = extract_text(filename)
+    text = text.encode('utf-8', errors='ignore').decode('utf-8')
+  if filetype == 'text/plain':
+    with open(filename, 'rb') as f:
+      text = f.read().decode('utf-8')
+  if filetype == 'text/html':
+    with open(filename, 'rb') as f:
+      soup = BeautifulSoup(f, 'html.parser')
+      text = soup.get_text()
+  
+  if os.path.exists(filename) and filename.find('content/') > -1:
+    os.remove(filename) 
     
-    text = ""
-    if filetype == 'text/html':
-        with open(filename, 'rb') as f:
-            soup = BeautifulSoup(f, 'html.parser')
-            text = soup.get_text()
-    
-    if os.path.exists(filename) and 'content/' in filename:
-        os.remove(filename)
-    
-    return text
+  return text
 
 def getconfig():
   config = configparser.ConfigParser()
